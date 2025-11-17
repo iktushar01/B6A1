6a03f09e6490aaffe05ce74b96ada58617a45e64
tushar <iktushar2020@gmail.com>
2025-11-17 21:19:50 +0600

    problem 2

--- solution.ts
+++ solution.ts
@@ -9,3 +9,16 @@ function formatValue(value : string | number | boolean) : string  |number | bool
         return !value;
     }
 }
+
+function getLength(value: string | any[]) : number {
+    if (typeof value === "string"){
+        return value.length;
+    }
+    else if ( Array.isArray(value)){
+        return value.length;
+    }
+    else {
+        return 0;
+    }
+}
+
